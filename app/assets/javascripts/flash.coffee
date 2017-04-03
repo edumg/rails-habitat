0ebf090612e98838dc379291b117711ce0f682cb
joao silva <j0a0.silva.kle@gmail.com>
2017-04-03 21:30:01 +0200

    cloudinary added to profile, added profile controller, added footer, enhanced flashes messages

---
+++ app/assets/javascripts/flash.coffee
@@ -0,0 +1,3 @@
+jQuery ->
+
+    $('#flash').hide().delay(80).fadeIn(800).delay(4000).fadeOut(800)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  resource :profile, only: [:show, :edit, :update]
+
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users'}
+
 end
