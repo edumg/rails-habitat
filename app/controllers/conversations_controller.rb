d6d7283a5a8a6433292c8a00150c1a327627b086
joao silva <j0a0.silva.kle@gmail.com>
2017-04-04 15:55:37 +0200

    mailboxer working with sending, mark as read, move to trash, dummy view, added to navbar

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,64 @@
+class ConversationsController < ApplicationController
+  before_filter :authenticate_user!
+  helper_method :mailbox, :conversation
+
+  def create
+    recipients = User.where(id: conversation_params[:recipients])
+    conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
+    flash[:success] = "Your message was successfully sent!"
+    redirect_to conversation_path(conversation)
+  end
+
+  # def show
+  #   conversation.mark_as_read(current_user)
+  # end
+
+  def reply
+    current_user.reply_conversation(conversation, *message_params(:body, :subject))
+    redirect_to conversation_path(conversation)
+  end
+
+  def trash
+    conversation.move_to_trash(current_user)
+    redirect_to mailbox_inbox_path
+  end
+
+  def mark_as_read
+    conversation.mark_as_read(current_user)
+    redirect_to mailbox_inbox_path
+  end
+
+  def untrash
+    conversation.untrash(current_user)
+    redirect_to :conversations
+  end
+
+  private
+
+  def mailbox
+    @mailbox ||= current_user.mailbox
+  end
+
+  def conversation_params
+    params.require(:conversation).permit(:subject, :body,recipients:[])
+  end
+
+  def conversation
+    #@conversation = mailbox.conversations
+    @conversation ||= mailbox.conversations.find(params[:id])
+  end
+
+  def message_params(*keys)
+    fetch_params(:message, *keys)
+  end
+
+  def fetch_params(key, *subkeys)
+    params[key].instance_eval do
+      case subkeys.size
+      when 0 then self
+      when 1 then self[subkeys.first]
+      else subkeys.map{|k| self[k] }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,19 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users'}
 
+  get "mailbox/inbox" => "mailbox#inbox", as: :mailbox_inbox
+  get "mailbox/sent"  => "mailbox#sent",  as: :mailbox_sent
+  get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
+  get "mailbox/mark_as_read" => "mailbox#mark_as_read", as: :mark_as_read
+
+  resources :conversations do
+    member do
+      post :reply
+      post :trash
+      post :untrash
+      post :mark_as_read
+    end
+  end
+
+  resources :conversations
 end
