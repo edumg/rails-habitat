c449b124c2bcdbb1afcbf5d80ee23d6d25cdfcf7
joao silva <j0a0.silva.kle@gmail.com>
2017-04-13 16:43:50 +0200

    changed personality controller done id question

--- app/controllers/personality_controller.rb
+++ app/controllers/personality_controller.rb
@@ -23,12 +23,11 @@ class PersonalityController < ApplicationController
     @results = params[:results].split(',')
 
     @results.each do |result|
-      puts result
       @answer = Answer.find_by photo: result
       new_person_storage = Personalitystorage.new
       new_person_storage.session = @session_id
       new_person_storage.answer_id = @answer.id
-      new_person_storage.question_id = 1
+      new_person_storage.question_id = Question.last.id
       new_person_storage.save
       raise
     end
