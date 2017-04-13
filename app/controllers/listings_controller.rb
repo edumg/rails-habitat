3d31c726f5b7791c98913d62d8e1f0ff634d679d
joao silva <j0a0.silva.kle@gmail.com>
2017-04-13 18:07:18 +0200

    google maps fixed

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -51,6 +51,10 @@ class ListingsController < ApplicationController
   def index
     #@listings = Listing.all
     @listings = Listing.where.not(latitude: nil, longitude: nil)
+    @hash = Gmaps4rails.build_markers(@listings) do |listing, marker|
+      marker.lat listing.latitude
+      marker.lng listing.longitude
+    end
   end
 
   private
